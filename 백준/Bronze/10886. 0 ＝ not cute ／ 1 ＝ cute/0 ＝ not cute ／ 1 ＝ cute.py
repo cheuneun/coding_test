84cb9250e0a68bc6613002c113d877e69b022448
cheuneun <haeun5364@gmail.com>
2026-01-20 15:48:10 +0900

    Delete 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute directory

--- 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-n = int(sys.stdin.readline())
-votes = [int(sys.stdin.readline()) for _ in range(n)]
-
-if votes.count(1) > votes.count(0):
-    print("Junhee is cute!")
-else:
-    print("Junhee is not cute!")
